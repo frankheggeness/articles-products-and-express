f1128a1e2743e4e212c8179e6fea36a279bfe641
Frank <fr.heggeness@gmail.com>
2019-04-23 16:57:55 -1000

    server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const PORT = 3000;
+const articles = require('./routes/articles.js');
+const products = require('./routes/products.js');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.use('/articles', articles);
+app.use('/products', products);
+
+const server = app.listen(PORT, () => {
+  console.log(`Express app is running at port ${PORT}`);
+});
