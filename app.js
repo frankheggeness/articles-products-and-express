4c432d442d9472f7436a7c50115c61687ea19835
Frank <fr.heggeness@gmail.com>
2019-04-27 19:19:00 -1000

    update

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const products = require('./routes/products.js');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const analytics = require('./middleware/analytics.js');
-// const router = express.Router();
+const methodOverride = require('method-override');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-const methodOverride = require('method-override');
 app.use(express.static('public'));
-
 app.use(methodOverride('_method'));
 app.use('/articles', articles);
 app.use('/products', products);
+
 app.get('/', (req, res) => {
   res.render('./templates/main');
 });
+
 app.get('/search', (req, res) => {
   res.render('./search');
 });
